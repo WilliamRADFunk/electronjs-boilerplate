a39fca9bad2eebdeeb591d95418ea0abcef887bb
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2019-03-02 02:19:24 -0500

    Implements the typescript version of the boilerplate.

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # electronjs-boilerplate
 (In Progress) - Simple "hello world" boilerplate application using ElectronJs.
+
+***
+
+## First Steps
+
+1. Navigate to the root folder where you want your project to reside.
+
+2. Run `git clone https://github.com/WilliamRADFunk/electronjs-boilerplate.git`
+
+3. Run `npm install`. If failure, see Common Gotchas section below.
+
+4. Change the name `electronjs-boilerplate` to the name of your choice in the `package.json` file (multiple locations in this file), the title in the `index.html` file, the `gulpfile.babel.js` file, and the `README.md` file.
+
+***
+
+## Classes
+
+Detailed [documentation](docs/README.md)
+
+***
+
+## Available Command Line Options
+
+`npm run build` will build your project, depositing all finished files in the created `dist/` folder.</br></br>
+
+`npm run start` will build the application, and launch it in the browser. Simply navigate to `http://localhost:8080`.</br></br>
+
+`npm run lint` will search the scss and typescript files for common style issues and alert you of any it finds.</br></br>
+
+`npm run readme` will update the docs folder with all off the typedoc-friendly commenting you've made in you typescript files.</br></br>
+
+`npm run test` won't work. It isn't implemented yet.</br></br>
+
+## New to Gulp
+
+-- Make sure to install Gulp at the global level, as this is a necessary step to make the boilerplate's scripts run.</br></br>
+
+`npm install -g gulp`
+
+***
+
+## Common Gotchas
+
+--Might get a failure to fully install when running `npm install`</br></br>
+
+Try running `npm install --ignore-scripts`</br></br>
+
+-- Might get the error</br>
+"Error: ENOENT: no such file or directory, scandir 'your-path/electronjs-boilerplate\node_modules\node-sass\vendor'"</br></br>
+
+To remedy this, simply run `npm rebuild node-sass`</br></br>
+
+-- If you're running the `npm run readme` command, and your classes are not all present.</br></br>
+
+Make sure you aren't importing a capitalized version of the name (ie. `import { Doug } from './Doug'` when it should in fact be `import { Doug } from './doug'`) assuming of cou
---
+++ src/ts/main.ts
@@ -0,0 +1,94 @@
+import { BrowserWindow, Tray } from 'electron';
+import * as path from 'path';
+/**
+ * Placeholder function typically used to initiate the applications loop.
+ */
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+	static tray: Electron.Tray;
+    static application: Electron.App;
+	static BrowserWindow: any;
+
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+	}
+	
+	private static showWindow() {
+		Main.mainWindow.show();
+		Main.mainWindow.focus();
+	};
+
+	// toggle window
+	private static toggleWindow() {
+		if (Main.mainWindow.isVisible()) {
+			Main.mainWindow.hide();
+		} else {
+			Main.showWindow();
+		}
+	};
+
+	private static createWindow() {
+		// Create the browser window.
+		Main.mainWindow = new Main.BrowserWindow({
+			width: 1200,
+			height: 800,
+			webPreferences: {
+				nodeIntegration: false,
+				webSecurity: false
+			}
+		});
+
+		Main.mainWindow.loadURL('file://' + __dirname + '/../../index.html');
+
+		// Open the DevTools.
+		Main.mainWindow.webContents.openDevTools()
+
+		// Emitted when the window is closed.
+		Main.mainWindow.on('closed', Main.onClose);
+
+		Main.mainWindow.once('ready-to-show', () => {
+			Main.mainWindow.show();
+			Main.mainWindow.focus();
+		});
+	};
+
+	private static onActivate() {
+        // On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (Main.mainWindow === null) {
+			Main.createWindow();
+		}
+    }
+
+    private static onClose() {
+        // Dereference the window object. 
+        Main.mainWindow = null;
+    }
+
+    private static onReady() {
+		// Create a new tray
+		Main.tray = new Tray(path.join(__dirname, '../../assets', 'images', 'electron-icon.png'));
+		Main.tray.on('right-click', Main.toggleWindow);
+		Main.tray.on('double-click', Main.toggleWindow);
+		Main.tray.on('click', function (event) {
+			Main.toggleWindow();
+		});
+
+		Main.createWindow();
+		Main.mainWindow.on('closed', Main.onClose);
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        // we pass the Electron.App object and the  
+        // Electron.BrowserWindow into this function 
+        // so this class has no dependencies. This 
+        // makes the code easier to write tests for 
+        Main.BrowserWindow = browserWindow;
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('ready', Main.onReady);
+        Main.application.on('activate', Main.onActivate);
+    }
+}
